c43a07b55a9e900320fe8a923572ec1a9521ee61
kioko <kiokotomas@gmail.com>
2014-10-15 22:39:53 +0300

    Created WelcomeScreen Activity
    Prompts the user to select an option

---
+++ AkirachixDroid/src/main/java/com/thomaskioko/akirachix/WelcomeScreen.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014. Thomas Kioko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.thomaskioko.akirachix;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ *
+ * @author kioko
+ */
+
+
+public class WelcomeScreen extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = WelcomeScreen.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weclome_screen);
+
+        Button btnRegister = (Button) findViewById(R.id.btnWelcomeRegister);
+        btnRegister.setOnClickListener(this);
+        Button btnLogin = (Button) findViewById(R.id.btnWelcomeLogin);
+        btnLogin.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.weclome_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnWelcomeRegister:
+                startActivity(new Intent(WelcomeScreen.this, RegisterActivity.class));
+                break;
+            case R.id.btnWelcomeLogin:
+                startActivity(new Intent(WelcomeScreen.this, LoginScreen.class));
+                break;
+            default:
+                Log.i(TAG, "@onClick: Error");
+                break;
+        }
+    }
+}
